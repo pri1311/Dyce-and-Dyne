d60c8151f5ea36db9a3ef010a845408d0eccfee3
DarshBavishi <bavishidarsh2001@gmail.com>
2021-10-27 13:33:38 +0530

    added tsp backend, UI, delivery routes

--- app.js
+++ app.js
@@ -70,124 +70,259 @@ app.use(function (req, res, next) {
 
 app.use(express.static(__dirname + "/public"));
 const geolib = require("geolib");
+const axios = require("axios");
 
-const no_of_children = 5;
-var cities = [0, 1, 2, 3, 4];
-const address = [
-  "Saibaba Nagar",
-  "Ram Nagar",
-  "Prem Nagar",
-  "MG Road",
-  "SV road",
-];
-var cordinates = [
-  { longitude: 72.845812, latitude: 19.21568 },
-  { longitude: 72.855673, latitude: 19.24558 },
-  { longitude: 72.852912, latitude: 19.25558 },
-  { latitude: 19.21558, longitude: 72.842812 },
-  { latitude: 19.21658, longitude: 72.843812 },
-];
+//======================= TSP FUNCTIONS  ========================================
+var NO_OF_CHILDREN = 0;
+var N = 0;
+var CITIES = [];
+var ADDRESS = [];
+var CORDINATES = [];
+var GENE_POOL = [];
+var PARENTS = [];
 
-var gene_pool = [
-  [0, 1, 2, 3, 4],
-  [4, 3, 2, 0, 1],
-  [3, 2, 4, 0, 1],
-  [1, 2, 4, 0, 3],
-  [0, 4, 1, 2, 3],
-];
-var parents = [];
-var total_cost = 100000000000;
-
-//Get dist:
+function create_initial_population() {
+  for (var i = 0; i < NO_OF_CHILDREN; i++) {
+    var tour = [];
+    for (var j = 0; j < N; j++) tour[j] = j;
+    tour.sort(() => Math.random() - 0.5);
+    while (PARENTS.includes(tour)) tour.sort(() => Math.random() - 0.5);
+    PARENTS.push(tour);
+    GENE_POOL.push(tour);
+  }
+}
 function calculate_fitness(arr) {
   var sum = 0;
   for (var i = 1; i < arr.length; i++)
     sum =
       sum +
-      geolib.getDistance(cordinates[arr[i]], cordinates[arr[i - 1]], 0.01);
+      geolib.getDistance(CORDINATES[arr[i]], CORDINATES[arr[i - 1]], 0.01);
   sum =
     sum +
-    geolib.getDistance(cordinates[0], cordinates[arr[arr.length - 1]], 0.01);
+    geolib.getDistance(CORDINATES[0], CORDINATES[arr[arr.length - 1]], 0.01);
   return sum;
 }
 function select_best_genes() {
-  let pq = [];
-  for (var i = 0; i < gene_pool.length; i++)
-    pq[i] = [calculate_fitness(gene_pool[i]), i];
-  pq.sort();
-  for (var i = 0; i < no_of_children; i++) parents[i] = gene_pool[pq[i][1]];
-  gene_pool = [];
-  for (var i = 0; i < no_of_children; i++) gene_pool[i] = parents[i];
-  console.log("Best genes: ");
-  for (var i = 0; i < no_of_children; i++)
-    console.log(gene_pool[i] + " %d", calculate_fitness(gene_pool[i]));
+  var pq = [];
+  for (var i = 0; i < GENE_POOL.length; i++)
+    pq[i] = [calculate_fitness(GENE_POOL[i]), i];
+  pq.sort(function (a, b) {
+    if (a[0] < b[0]) return -1;
+    else if (a[0] > b[0]) return 1;
+    return 0;
+  });
+  for (var i = 0; i < NO_OF_CHILDREN; i++) PARENTS[i] = GENE_POOL[pq[i][1]];
+  GENE_POOL = [];
+  for (var i = 0; i < NO_OF_CHILDREN; i++) GENE_POOL[i] = PARENTS[i];
 }
-function getChild(p1, p2, len) {
+function getChild(p1, p2, l) {
   let c1 = [],
     c2 = [];
-  for (var i = 0; i < p1.length; i++) {
+  for (var i = 0; i < N; i++) {
     c1[i] = -1;
     c2[i] = -1;
   }
-
-  let start = Math.floor(p1.length / 2 - 1);
-  for (var i = start; i <= start + len; i++) {
-    c1[i] = p2[i];
-    c2[i] = p1[i];
+  let start = Math.floor(N / 2) - 1;
+  for (var i = start; i <= start + l; i++) {
+    c1[i] = p1[i];
+    c2[i] = p2[i];
   }
+  var temp = [];
+  for (var i = 0; i < N; i++) if (!c1.includes(p2[i])) temp.push(p2[i]);
+  let index = 0;
+  for (var i = 0; i < N; i++) if (c1[i] == -1) c1[i] = temp[index++];
+  temp = [];
+  for (var i = 0; i < N; i++) if (!c2.includes(p1[i])) temp.push(p1[i]);
+  index = 0;
+  for (var i = 0; i < N; i++) if (c2[i] == -1) c2[i] = temp[index++];
 
-  for (var i = 0; i < p2.length; i++) if (!c1.includes(p2[i])) c1[i] = p2[i];
-  for (var i = 0; i < p1.length; i++) if (!c2.includes(p1[i])) c2[i] = p1[i];
   return [c1, c2];
 }
 function crossover() {
-  //Order length=log(parents)/log2
-  const len = Math.log2(parents[0].length);
+  const len = Math.log2(N);
   offsprings = [];
-  for (var i = 0; i < no_of_children; i++) {
-    for (var j = 0; j < no_of_children; j++) {
+  for (var i = 0; i < NO_OF_CHILDREN; i++) {
+    for (var j = 0; j < NO_OF_CHILDREN; j++) {
       if (i != j) {
-        let children = getChild(parents[i], parents[j], len);
-        gene_pool.push(children[0]);
-        gene_pool.push(children[1]);
+        let children = getChild(PARENTS[i], PARENTS[j], len);
+
+        GENE_POOL.push(children[0]);
+        GENE_POOL.push(children[1]);
       }
     }
   }
 }
+function mutation() {
+  for (var i = 0; i < Math.floor(Math.log2(NO_OF_CHILDREN)); i++) {
+    for (var j = 0; j < 2; j++) {
+      let index = Math.floor(Math.random() * NO_OF_CHILDREN);
+      let el1 = Math.floor(Math.random() * N);
+      let el2 = Math.floor(Math.random() * N);
+      while (el2 == el1) el2 = Math.floor(Math.random() * N);
+      let x = GENE_POOL[index][el1];
+      GENE_POOL[index][el1] = GENE_POOL[index][el2];
+      GENE_POOL[index][el2] = x;
+    }
+  }
+}
 function genetic_TSP() {
-  //create_population()
+  create_initial_population();
   let iterations = 0;
-  while (iterations < 5) {
+  let limit = N ** (5 / 2);
+  while (iterations < limit) {
     select_best_genes();
     crossover();
+    if (iterations % 20 == 0) mutation();
     iterations++;
   }
   select_best_genes();
-  console.log(parents[no_of_children - 1]);
+  return [PARENTS[0], calculate_fitness(PARENTS[0])];
 }
-
-app.get("/tsp", function (req, res) {
-  genetic_TSP();
-  let warehouseLocation = [];
-  let location = [];
-  let order = parents[no_of_children - 1];
-  for (var i = 0; i < order.length; i++) {
-    console.log(address[order[i]]);
-    location.push(address[order[i]]);
-    warehouseLocation.push([
-      cordinates[order[i]].longitude,
-      cordinates[order[i]].latitude,
-    ]);
+const getAdd = async (add) => {
+  try {
+    return await axios.get(
+      "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+        add +
+        ".json?access_token=pk.eyJ1IjoiYmhhdnlhMDkyIiwiYSI6ImNrb2xlNWUzbjFiNjAydnE0bXpsbG13MGcifQ.jik4Q9NnhmABl1qhYKt7ZA"
+    );
+  } catch (error) {
+    console.error(error);
   }
-  console.log(location);
-  console.log(warehouseLocation);
-  res.render("tsp.ejs", {
-    warehouseLocation: warehouseLocation,
-    order: location,
+};
+const _addressToLatLng = async (address) => {
+  const resp = await getAdd(address);
+  if (resp.data) {
+    var lng = resp["data"]["features"][0].geometry.coordinates[0];
+    var lat = resp["data"]["features"][0].geometry.coordinates[1];
+
+    return { longitude: lng, latitude: lat };
+  }
+};
+
+//=============================== ROUTES ===============================
+
+app.get("/tsp/:deliveryNo", async function (req, res) {
+  var currdt = Date.now();
+  Order.find({}, async function (err, allOrders) {
+    if (err) console.log(err);
+    else {
+      var map = {};
+      allOrders.forEach((order) => {
+        var diff = currdt - order.date;
+        diff = diff / 1000;
+
+        var x = Math.ceil(diff / 1200);
+        if (map[x] === undefined) map[x] = [];
+        map[x].push(order);
+      });
+      var schedule = [];
+      var cost = [];
+      for (var key in map) {
+        var temp = [];
+        for (var a in map[key]) {
+          temp.push(map[key][a]);
+        }
+        schedule.push(temp);
+      }
+      var allOrders = schedule[req.params.deliveryNo];
+
+      NO_OF_CHILDREN = 0;
+      N = 0;
+      CITIES = [];
+      ADDRESS = [];
+      CORDINATES = [];
+      GENE_POOL = [];
+      PARENTS = [];
+      N = allOrders.length + 1;
+      NO_OF_CHILDREN = (3 * N) / 2;
+      var i = 0;
+      for (i = 0; i < allOrders.length; i++) {
+        ADDRESS.push(allOrders[i].user.address);
+        CITIES.push(i);
+        var x = await _addressToLatLng(allOrders[i].user.address);
+        CORDINATES.push(x);
+      }
+      ADDRESS.push("Mahavir Nagar, Borivali west, Mumbai");
+      CORDINATES.push({ longitude: 72.84167, latitude: 19.21333 });
+      CITIES.push(i);
+      var bestTour;
+      var minCost = 100000000;
+
+      for (var i = 0; i < 10; i++) {
+        var x = genetic_TSP();
+        var tour = x[0];
+        var cost = x[1];
+
+        console.log(tour, cost);
+
+        if (cost < minCost) {
+          minCost = cost;
+          bestTour = tour.slice();
+        }
+      }
+      console.log(bestTour, calculate_fitness(bestTour));
+      i = 0;
+      for (i = 0; i < N; i++) if (bestTour[i] == N - 1) break;
+
+      var finalTour = [];
+      i = (i + 1) % N;
+      for (var j = 0; j < N - 1; j++) {
+        finalTour[j] = bestTour[i];
+        i = (i + 1) % N;
+      }
+      bestTour = finalTour;
+      let cords = [];
+      let location = [];
+      for (var i = 0; i < N - 1; i++) {
+        console.log(ADDRESS[bestTour[i]]);
+        location.push(ADDRESS[bestTour[i]]);
+        cords.push([
+          CORDINATES[bestTour[i]].longitude,
+          CORDINATES[bestTour[i]].latitude,
+        ]);
+      }
+      res.render("tsp.ejs", { cords: cords, order: location });
+    }
   });
 });
 
-app.get("/", isLoggedIn, (req, res) => {
+app.get("/delivery", function (req, res) {
+  var currdt = Date.now();
+  Order.find({}, function (err, allOrders) {
+    if (err) console.log(err);
+    else {
+      var map = {};
+      allOrders.forEach((order) => {
+        var diff = currdt - order.date;
+        diff = diff / 1000;
+
+        var x = Math.ceil(diff / 1200);
+        if (map[x] === undefined) map[x] = [];
+        map[x].push(order);
+      });
+      var schedule = [];
+      var cost = [];
+      for (var key in map) {
+        var temp = [];
+        var sum = 0;
+        for (var a in map[key]) {
+          sum = sum + map[key][a]["total"];
+          temp.push(map[key][a]);
+        }
+        cost.push(sum);
+        schedule.push(temp);
+      }
+      console.log(cost);
+      res.render("delivery", { schedule: schedule, cost: cost });
+    }
+  }).sort({ date: -1 });
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/home", isLoggedIn, (req, res) => {
   res.render("home");
 });
 
